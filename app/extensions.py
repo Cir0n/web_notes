de0ac2a2a2e898b1b4d75d645d62f5b1e2cdba6e
Cir0n <ciron@desktop-Ciron.home>
2025-02-07 10:00:05 +0100

    ajout de secret key

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,17 @@
+import os
+
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 
+load_dotenv()
+
+
 bcrypt = Bcrypt()
+
+ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
+
+if not ENCRYPTION_KEY:
+    raise ValueError("No encryption key set for the application")
+
+CIPHER = Fernet(ENCRYPTION_KEY.encode())
